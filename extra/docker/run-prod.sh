d7fca238c3c166a6f7a2735e24c8626e1fd44a1a
Calogero Crapanzano <calosan@libero.it>
2019-07-02 16:04:08 +0200

    gaia docker with volume

--- extra/docker/run-prod.sh
+++ extra/docker/run-prod.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo docker run -it -p 1881:1880 -e DEST=prod --name gaia-prod exentriq/node-red-gaia
+SPACE=$1
+PORT=$2
+VOLUME=GAIA_SPACE_$1
+sudo docker run -it --mount source=$VOLUME,target=/data -p $PORT:1880 -e DEST=prod --name gaia-prod-space$SPACE exentriq/node-red-gaia
--- extra/docker/run-stage.sh
+++ extra/docker/run-stage.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo docker run -it -p 1880:1880 --name gaia-stage exentriq/node-red-gaia
+SPACE=$1
+PORT=$2
+VOLUME=GAIA_SPACE_$1
+sudo docker run -it --mount source=$VOLUME,target=/data -p $PORT:1880 --name gaia-stage-space$SPACE exentriq/node-red-gaia
