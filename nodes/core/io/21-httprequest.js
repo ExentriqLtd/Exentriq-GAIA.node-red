4bc2cc9ec06be5021474b0cf23a6dd8c9474b383
Calogero Crapanzano <calogero.crapanzano@unipa.it>
2021-03-17 11:33:48 +0100

    Fix Http Request node

--- nodes/core/io/21-httprequest.js
+++ nodes/core/io/21-httprequest.js
@@ -142,7 +142,12 @@ module.exports = function(RED) {
             }
             if (tlsNode) {
                 tlsNode.addTLSOptions(opts);
+            } else {
+                if (msg.hasOwnProperty('rejectUnauthorized')) {
+                    opts.rejectUnauthorized = msg.rejectUnauthorized;
+                }
             }
+        
             var req = ((/^https/.test(urltotest))?https:http).request(opts,function(res) {
                 (node.ret === "bin") ? res.setEncoding('binary') : res.setEncoding('utf8');
                 msg.statusCode = res.statusCode;
